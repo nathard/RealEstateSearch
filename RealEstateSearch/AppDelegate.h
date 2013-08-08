fff8029c9ae77255621742e4b747e02aecc9ee1f
nathard <nathard@gmail.com>
2013-08-08 15:16:09 +1000

    Initial Sync

---
+++ RealEstateSearch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RealEstateSearch
+//
+//  Created by NATHAN HARDING on 3/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RealEstateSearch/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RealEstateSearch
+//
+//  Created by NATHAN HARDING on 3/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
